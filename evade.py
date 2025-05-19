26b8384c43c8adccd63e39b6ab550fcbb563c12f
Tobbe <161828477+Tobeeeeeeeee@users.noreply.github.com>
2025-05-19 12:10:00 +0200

    Lägger till färger och spelarens startposition

--- evade.py
+++ evade.py
@@ -9,3 +9,15 @@ pygame.init()
 WIDTH, HEIGHT = 400, 600
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("evade!")
+
+# Färger
+DARK_BG = (30, 30, 30)
+LIME_GREEN = (50, 205, 50)
+PINK = (255, 105, 180)
+BLACK = (0, 0, 0)
+
+# Spelare
+player_size = 25
+player_x = WIDTH // 2 - player_size // 2
+player_y = HEIGHT - player_size - 50
+player_speed = 5
